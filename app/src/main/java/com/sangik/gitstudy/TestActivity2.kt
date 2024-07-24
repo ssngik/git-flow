a6b3a2f8e36f0ef6b891ab7276bf7534190e358d
김상익 <ksik518@naver.com>
2024-07-24 20:03:10 +0900

    feat: add test log

--- app/src/main/java/com/sangik/gitstudy/TestActivity2.kt
+++ app/src/main/java/com/sangik/gitstudy/TestActivity2.kt
@@ -2,10 +2,13 @@ package com.sangik.gitstudy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class TestActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test2)
+
+        Log.d("TEST", "TEST LOG")
     }
 }
